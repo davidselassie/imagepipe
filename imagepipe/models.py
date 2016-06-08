59a565201396fcbc69177a330b055bb4fb181488
David Selassie <selassid@gmail.com>
2016-06-08 16:11:01 -0700

    Ensures unique filenames.

--- imagepipe/models.py
+++ imagepipe/models.py
@@ -1,11 +1,36 @@
 """imagepipe Models."""
+import posixpath
+import uuid
+
 from django.db import models
+
+
+def unique_mashup(_, filename):
+    """Return a path in the mashups directory with a unique prefix.
+
+    This ensures no two files will overwrite each other.
+
+    This has to be a plain function for migration serialization.
+    """
+    random_prefix = str(uuid.uuid4()) + '_'
+    return posixpath.join('mashups', random_prefix + filename)
+
+
+def unique_source(_, filename):
+    """Return a path in the mashups directory with a unique prefix.
+
+    This ensures no two files will overwrite each other.
+
+    This has to be a plain function for migration serialization.
+    """
+    random_prefix = str(uuid.uuid4()) + '_'
+    return posixpath.join('sources', random_prefix + filename)
 
 
 class Mashup(models.Model):
     """Stores a mashed-up combination of two images."""
 
-    image_file = models.ImageField(upload_to='mashups')
+    image_file = models.ImageField(upload_to=unique_mashup)
     title = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -18,7 +43,7 @@ class Mashup(models.Model):
 class Source(models.Model):
     """Stores an unmodified user-uploaded image."""
 
-    image_file = models.ImageField(upload_to='sources')
+    image_file = models.ImageField(upload_to=unique_source)
     title = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     mashup = models.ForeignKey(
